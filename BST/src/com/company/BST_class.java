735ca0aca989a95d52a0f621f6b2233f3740215a
yerasylbekenov <77799107+yerasylbekenov@users.noreply.github.com>
2021-06-04 16:11:42 +0600

    Update BST_class.java

--- BST/src/com/company/BST_class.java
+++ BST/src/com/company/BST_class.java
@@ -1,5 +1,103 @@
 package com.company;
 
 public class BST_class {
+
+    class Node {
+        int key;
+        Node left, right;
+
+        public Node(int data){
+            key = data;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+
+    BST_class(){
+        root = null;
+    }
+
+    void deleteKey(int key) {
+        root = delete_Recursive(root, key);
+    }
+
+
+    Node delete_Recursive(Node root, int key)  {
+
+        if (root == null)  return root;
+
+
+        if (key < root.key)     
+            root.left = delete_Recursive(root.left, key);
+        else if (key > root.key)  
+            root.right = delete_Recursive(root.right, key);
+        else  {
+
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+
+            root.key = minValue(root.right);
+
+
+            root.right = delete_Recursive(root.right, root.key);
+        }
+        return root;
+    }
+
+    int minValue(Node root)  {
+        //initially minval = root
+        int minval = root.key;
+        //find minval
+        while (root.left != null)  {
+            minval = root.left.key;
+            root = root.left;
+        }
+        return minval;
+    }
+
+    void insert(int key)  {
+        root = insert_Recursive(root, key);
+    }
+
+    Node insert_Recursive(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key)     //insert in the left subtree
+            root.left = insert_Recursive(root.left, key);
+        else if (key > root.key)    //insert in the right subtree
+            root.right = insert_Recursive(root.right, key);
+
+        return root;
+    }
+
+    void inorder() {
+        inorder_Recursive(root);
+    }
+
+    void inorder_Recursive(Node root) {
+        if (root != null) {
+            inorder_Recursive(root.left);
+            System.out.print(root.key + " ");
+            inorder_Recursive(root.right);
+        }
+    }
     
+    Node search_Recursive(Node root, int key)  {
+
+        if (root==null || root.key==key)
+            return root;
+
+        if (root.key > key)
+            return search_Recursive(root.left, key);
+
+        return search_Recursive(root.right, key);
+    }
 }
